856ff0fcc5ec030c1f72c28fe0ecc61dbdd9102d
Grigoriy Khlytin <xlootin@niuitmo.ru>
2022-03-14 07:59:41 +0300

    hw3: CommonDAO: removing common code

--- refactoring/src/main/java/ru/akirakozov/sd/refactoring/dao/CommonDAO.java
+++ refactoring/src/main/java/ru/akirakozov/sd/refactoring/dao/CommonDAO.java
@@ -7,122 +7,70 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CommonDAO {
-    public static void createProductTable() throws SQLException {
-        try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-            String sql = "CREATE TABLE IF NOT EXISTS PRODUCT" +
-                    "(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
-                    " NAME           TEXT    NOT NULL, " +
-                    " PRICE          INT     NOT NULL)";
-            Statement stmt = c.createStatement();
+    private static final String DB_URL = "jdbc:sqlite:test.db";
 
+    private static void executeUpdate(String sql) throws SQLException {
+        try (Connection c = DriverManager.getConnection(DB_URL)) {
+            Statement stmt = c.createStatement();
             stmt.executeUpdate(sql);
             stmt.close();
         }
+    }
+
+    private static List<Product> executeQuery(String sql, boolean isScalar) throws SQLException {
+        List<Product> result = new ArrayList<>();
+        try (Connection c = DriverManager.getConnection(DB_URL)) {
+            Statement stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                if (isScalar) {
+                    result.add(new Product("", rs.getInt(1)));
+                } else {
+                    result.add(new Product(rs.getString("name"), rs.getInt("price")));
+                }
+            }
+            rs.close();
+            stmt.close();
+        }
+        return result;
+    }
+
+    public static void createProductTable() throws SQLException {
+        String sql = "CREATE TABLE IF NOT EXISTS PRODUCT" +
+                "(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
+                " NAME           TEXT    NOT NULL, " +
+                " PRICE          INT     NOT NULL)";
+        executeUpdate(sql);
     }
 
     public static void insertIntoProductTable(String name, long price) throws SQLException {
-        try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-            String sql = "INSERT INTO PRODUCT " +
-                    "(NAME, PRICE) VALUES (\"" + name + "\"," + price + ")";
-            Statement stmt = c.createStatement();
-            stmt.executeUpdate(sql);
-            stmt.close();
-        }
+        String sql = "INSERT INTO PRODUCT " +
+                "(NAME, PRICE) VALUES (\"" + name + "\"," + price + ")";
+        executeUpdate(sql);
     }
 
     public static List<Product> selectAllFromProductTable() throws SQLException {
-        List<Product> result = new ArrayList<>();
-
-        try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-            Statement stmt = c.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM PRODUCT");
-
-            while (rs.next()) {
-                String name = rs.getString("name");
-                int price = rs.getInt("price");
-                result.add(new Product(name, price));
-            }
-
-            rs.close();
-            stmt.close();
-        }
-
-        return result;
+        String sql = "SELECT * FROM PRODUCT";
+        return executeQuery(sql, false);
     }
 
     public static List<Product> selectFromProductTableByMax() throws SQLException {
-        List<Product> result = new ArrayList<>();
-
-        try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-            Statement stmt = c.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM PRODUCT ORDER BY PRICE DESC LIMIT 1");
-
-            while (rs.next()) {
-                String name = rs.getString("name");
-                int price = rs.getInt("price");
-                result.add(new Product(name, price));
-            }
-
-            rs.close();
-            stmt.close();
-        }
-
-        return result;
+        String sql = "SELECT * FROM PRODUCT ORDER BY PRICE DESC LIMIT 1";
+        return executeQuery(sql, false);
     }
 
     public static List<Product> selectFromProductTableByMin() throws SQLException {
-        List<Product> result = new ArrayList<>();
-
-        try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-            Statement stmt = c.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM PRODUCT ORDER BY PRICE LIMIT 1");
-
-            while (rs.next()) {
-                String name = rs.getString("name");
-                int price = rs.getInt("price");
-                result.add(new Product(name, price));
-            }
-
-            rs.close();
-            stmt.close();
-        }
-
-        return result;
+        String sql = "SELECT * FROM PRODUCT ORDER BY PRICE LIMIT 1";
+        return executeQuery(sql, false);
     }
 
     public static int selectFromProductTableBySum() throws SQLException {
-        int result = 0;
-
-        try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-            Statement stmt = c.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT SUM(price) FROM PRODUCT");
-
-            if (rs.next()) {
-                result = rs.getInt(1);
-            }
-
-            rs.close();
-            stmt.close();
-        }
-
-        return result;
+        String sql = "SELECT SUM(price) FROM PRODUCT";
+        return (int) executeQuery(sql, true).get(0).price;
     }
 
     public static int selectFromProductTableByCount() throws SQLException {
-        int result = 0;
-
-        try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-            Statement stmt = c.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT COUNT(*) FROM PRODUCT");
-
-            if (rs.next()) {
-                result = rs.getInt(1);
-            }
-
-            rs.close();
-            stmt.close();
-        }
-
-        return result;
+        String sql = "SELECT COUNT(*) FROM PRODUCT";
+        return (int) executeQuery(sql, true).get(0).price;
     }
 }
